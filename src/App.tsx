742bf033445ff170a19c03eee1316ceaa7094bed
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2024-10-07 17:38:09 +0700

    feat: colors on tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
     <>
       <section className="flex justify-center items-center flex-col h-screen gap-6 bg-black">
-        <h1 className="text-3xl text-red-400 font-semibold">
+        <h1 className="text-3xl text-webstyle-primary font-semibold">
           Hello world React Ts!
         </h1>
-        <div className="flex gap-3">
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="w-16" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="w-16" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React Ts</h1>
-        <div className="flex justify-center flex-col items-center">
-          <button
-            className="bg-[#1a1a1a] text-white px-3 py-2 rounded-md focus:border-2 focus:border-blue-500 max-w-[140px]"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </button>
-          <p className="text-white">
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
         <a
           href="https://github.com/adityaajinug"
-          className="hover:text-red-500 font-semibold text-white"
+          className="hover:text-red-500 font-semibold text-white underline"
         >
           Aditya Ajie Nugroho
         </a>
@@ -42,5 +15,3 @@ function App() {
     </>
   );
 }
-
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./src/**/*.{html,js,tsx,ts,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        webstyle: {
+          primary: "#299D91",
+          secondary: "#191919",
+          grey: {
+            500: "#525256",
+            400: "#666666",
+            300: "#878787",
+            200: "#9F9F9F",
+            100: "#E8E8E8",
+            50: "#F3F3F3",
+          },
+          addon: {
+            danger: "#E73D1C",
+            success: "#4DAF6E",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
