92d792e4be60a5c29f4df12fc8e7587028b0a3fd
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2024-11-04 16:37:17 +0700

    fix: mainLayout

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,6 +1,11 @@
 import { Sidebar } from '@/components/Fragments/Sidebar';
 import { TopBar } from '@/components/Fragments/TopBar';
-export const MainLayout: React.FC = () => {
+
+interface MainLayoutProps {
+    childern: React.ReactNode
+}
+export const MainLayout: React.FC<MainLayoutProps> = (props) => {
+    const {childern} = props
   return (
     <>
         <div className="flex min-w-full">
@@ -9,38 +14,7 @@ export const MainLayout: React.FC = () => {
            <div className="flex flex-col gap-4 w-full">
                 <TopBar />
                 <div className="px-8 py-5">
-                    <div className="grid grid-cols-12 gap-5">
-                        <div className="col-span-12 xl:col-span-4">
-                            <div className="shadow-lg px-5 py-6 w-full">
-                                Deskripsi 1
-                            </div>
-                        </div>
-                        <div className="col-span-12 xl:col-span-4">
-                            <div className="shadow-lg px-6 py-6 w-full">
-                                Deskripsi 1
-                            </div>
-                        </div>
-                        <div className="col-span-12 xl:col-span-4">
-                            <div className="shadow-lg px-5 py-6 w-full">
-                                Deskripsi 1
-                            </div>
-                        </div>
-                        <div className="col-span-12 xl:col-span-4">
-                            <div className="shadow-lg px-5 py-6 w-full">
-                                Deskripsi 1
-                            </div>
-                        </div>
-                        <div className="col-span-12 xl:col-span-4">
-                            <div className="shadow-lg px-5 py-6 w-full">
-                                Deskripsi 1
-                            </div>
-                        </div>
-                        <div className="col-span-12 xl:col-span-4">
-                            <div className="shadow-lg px-5 py-6 w-full">
-                                Deskripsi 1
-                            </div>
-                        </div>
-                    </div>
+                    {childern}
                 </div>
            </div>
 
