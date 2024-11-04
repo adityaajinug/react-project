690b89d1dc48643982a5c94e37275f68107c3429
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2024-11-04 16:40:52 +0700

    fix: dashboard page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,10 +1,24 @@
+import { CardTitle } from '@/components/Elements/Card/CardTitle';
 import { MainLayout } from '@/components/Layouts/MainLayout';
 import React from 'react'
 
 const DashboardPage: React.FC = () => {
     return (
         <>
-            <MainLayout />
+            <MainLayout childern={
+                <div className="grid grid-cols-12 gap-5">
+                    <div className="col-span-12 xl:col-span-4">
+                        <CardTitle cardTitle="Deskripsi 1" cardText="Deskripsi." />
+                    </div>
+                    <div className="col-span-12 xl:col-span-4">
+                        <CardTitle cardTitle="Deskripsi 2" cardText="Deksripsi 2" />
+                    </div>
+                    <div className="col-span-12 xl:col-span-4">
+                        <CardTitle cardTitle="Deskripsi 3" cardText="Deskripsi 3" />
+                    </div>
+                </div>
+            }> 
+            </MainLayout>
         </>
     )
 }
