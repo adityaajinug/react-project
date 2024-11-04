8ffcab45e0fcfea99a6594fef8e74a0120497395
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2024-11-04 15:21:29 +0700

    feat: content and topbar

--- src/components/Elements/Logo/Index.tsx
+++ src/components/Elements/Logo/Index.tsx
@@ -6,7 +6,7 @@ const Logo: React.FC<LogoType> = (props) => {
     const { className = 'text-webstyle-primary' } = props;
     return (
         <>
-        <h1 className={`${className} font-poppins text-base xl:text-4xl font-extrabold text-center`}>
+        <h1 className={`${className} font-poppins text-base xl:text-3xl font-extrabold text-center`}>
             FINE
             <span className="font-medium font-poppins">bank.</span>
             IO
--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -2,7 +2,8 @@ import Logo from '@/components/Elements/Logo/Index'
 import React from 'react'
 import { iconAssets } from '@/datas/IconAssets'
 import { MenuItem } from '@/types/menu'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 export const MainLayout: React.FC = () => {
     const menuItems: MenuItem[] = [
         { icon: iconAssets.overview, title: "Overview" },
@@ -15,10 +16,12 @@ export const MainLayout: React.FC = () => {
     ];
   return (
     <>
-        <div className="flex gap-6">
-            <div className="flex flex-col justify-between gap-16 bg-webstyle-secondary px-4 py-12 xl:px-7 xl:py-12 min-h-screen overflow-y-auto w-fit">
-                <div className="flex flex-col gap-10">
-                    <Logo className="text-webstyle-grey-0 max-w-28 xl:max-w-[224px]" />
+        <div className="flex min-w-full">
+            <div className="flex flex-col justify-between gap-16 bg-webstyle-secondary px-4 py-12 xl:px-7 xl:py-12 min-h-screen overflow-y-auto w-fit xl:w-[280px]">
+                <div className="flex flex-col gap-10 w-fit">
+                    <div className="max-w-28">
+                        <Logo className="text-webstyle-grey-0 w-full" />
+                    </div>
                     <div className="flex flex-col gap-4">
                     {menuItems.map((item, index) => (
                         <a href="#" key={index} className="hover:bg-white hover:bg-opacity-10 items-center justify-center xl:justify-start active:bg-webstyle-primary flex gap-3 py-4 px-3 rounded">
@@ -50,15 +53,64 @@ export const MainLayout: React.FC = () => {
                         <a href="#" className="max-w-6 hidden xl:block">
                             <img src={iconAssets.bullets} alt="icon bullets" />
                         </a>
-
-
                     </div>
                 </div>
             </div>
             
-            <div className="px-8 py-5">
+           <div className="flex flex-col gap-4 w-full">
+                <div className="px-8 py-5 flex justify-between border-b border-gray-300 w-full h-fit">
+                        <div className="flex gap-6 items-center">
+                            <h1 className="font-bold text-2xl">Hello Tanzir</h1>
+                            <p className="">May 19, 2023</p>
+                        </div>
+                        <div className="flex items-center gap-10">
+                            <FontAwesomeIcon icon={faBell}/>
+                            <form action="#">
+                                <div className="bg-white shadow-md rounded-md">
+                                    <input type="text" placeholder="Search" className="px-6 py-3 border-none outline-none" />
 
-            </div>
+                                </div>
+                            </form>
+                        </div>
+
+                </div>
+                <div className="px-8 py-5">
+                    <div className="grid grid-cols-12 gap-5">
+                        <div className="col-span-12 xl:col-span-4">
+                            <div className="shadow-lg px-5 py-6 w-full">
+                                Deskripsi 1
+                            </div>
+                        </div>
+                        <div className="col-span-12 xl:col-span-4">
+                            <div className="shadow-lg px-6 py-6 w-full">
+                                Deskripsi 1
+                            </div>
+                        </div>
+                        <div className="col-span-12 xl:col-span-4">
+                            <div className="shadow-lg px-5 py-6 w-full">
+                                Deskripsi 1
+                            </div>
+                        </div>
+                        <div className="col-span-12 xl:col-span-4">
+                            <div className="shadow-lg px-5 py-6 w-full">
+                                Deskripsi 1
+                            </div>
+                        </div>
+                        <div className="col-span-12 xl:col-span-4">
+                            <div className="shadow-lg px-5 py-6 w-full">
+                                Deskripsi 1
+                            </div>
+                        </div>
+                        <div className="col-span-12 xl:col-span-4">
+                            <div className="shadow-lg px-5 py-6 w-full">
+                                Deskripsi 1
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+
+           
         </div>
     </>
   )
