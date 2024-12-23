b5f1e87bd53fbdd9b5a1198c10d968b18b4e731b
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2024-12-23 18:00:44 +0700

    fix: remove use State

--- src/components/Fragments/Sidebar.tsx
+++ src/components/Fragments/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { iconAssets } from '@/datas/IconAssets'
 import Logo from '@/components/Elements/Logo/Index';
 import { Link } from 'react-router-dom';
